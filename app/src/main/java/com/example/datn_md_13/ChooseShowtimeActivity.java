3432af10b6acc2681193486cc1cb962737ba3648
NGUYENBINHAN\DELL <annbph34166@fpt.edu.vn>
2025-10-24 11:37:55 +0700

    An giao dien

---
+++ app/src/main/java/com/example/datn_md_13/ChooseShowtimeActivity.java
@@ -0,0 +1,6 @@
+package com.example.datn_md_13;
+
+import android.app.Activity;
+
+public class ChooseShowtimeActivity extends Activity {
+}
---
+++ app/src/main/java/com/example/datn_md_13/MovieDetailActivity.java
@@ -0,0 +1,6 @@
+package com.example.datn_md_13;
+
+import android.app.Activity;
+
+public class MovieDetailActivity extends Activity {
+}
