6bb13af4ea5c039738cef8d56d7c20f1f4dbd6a5
Sao123451 <saondph35070@fpt.edu.vn>
2025-10-29 11:17:24 +0700

    Sao check funtion crud movies

--- app.js
+++ app.js
@@ -674,25 +674,3 @@ function renderMoviesPage(container) {
   load();
 }
 
-
-
-// app.js cu cua Sao
-
-// // helpers
-// const $ = (s, r = document) => r.querySelector(s);
-
-// // render login by default
-// (function start() {
-//   const tpl = $('#tpl-login');
-//   if (!tpl) return console.error('Không tìm thấy template login');
-//   const frag = tpl.content.cloneNode(true);
-//   const root = $('#root');
-//   root.innerHTML = '';
-//   root.append(frag);
-
-//   // demo button
-//   $('#btn-fill-demo').onclick = () => {
-//     $('#lg-username').value = 'admin@example.com';
-//     $('#lg-password').value = 'admin123';
-//   };
-// })();
