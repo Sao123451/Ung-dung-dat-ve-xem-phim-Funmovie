a543c00a1c2510febee6bc2eb1581b6d826b956a
Sao123451 <saondph35070@fpt.edu.vn>
2025-11-15 00:56:14 +0700

    Sao sửa movies

--- models/Movie.js
+++ models/Movie.js
@@ -3,18 +3,23 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const MovieSchema = new Schema({
-  title: { type: String, required: true },
-  description: String,  
-  duration: Number,
-  director: String,
-  cast: [String],
-  genre: [String],
+  title:        { type: String, required: true },
+  description:  String,
+  duration:     Number,
+  director:     String,
+  cast:         [String],
+  genre:        [String],
   release_date: Date,
-  language: String,
-  poster: String,
-  rating: Number,
-  status: { type: String, enum: ['coming','now_showing','archived'], default: 'coming' },
-  created_at: { type: Date, default: Date.now }
+  language:     String,
+  poster:       String,
+  rating:       Number,
+
+  // ⭐ GIỚI HẠN ĐỘ TUỔI
+  // 0 = phổ biến, 13 = T13, 16 = T16, 18 = T18
+  age_limit:    { type: Number, default: 0 },
+
+  status:       { type: String, enum: ['coming','now_showing','archived'], default: 'coming' },
+  created_at:   { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('Movie', MovieSchema);
